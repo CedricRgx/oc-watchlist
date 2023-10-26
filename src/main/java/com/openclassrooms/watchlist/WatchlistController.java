579ebdc9377f6bf15f0a234a2be789f789dc845d
CedricRgx <134865690+CedricRgx@users.noreply.github.com>
2023-10-26 18:51:32 +0200

    Ajout du formulaire (Get/Post) et les tests unitaires associés

--- src/main/java/com/openclassrooms/watchlist/WatchlistController.java
+++ src/main/java/com/openclassrooms/watchlist/WatchlistController.java
@@ -7,28 +7,69 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WatchlistController {
 
     List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
+    private static int index = 1;
+
+    @GetMapping("/watchlistItemForm")
+    public ModelAndView showWatchlistItemForm(@RequestParam(required=false) Integer id) {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        WatchlistItem watchlistItem = findWatchlistItemById(id);
+        if (watchlistItem == null) {
+            model.put("watchlistItem", new WatchlistItem());
+        } else {
+            model.put("watchlistItem", watchlistItem);
+        }
+        return new ModelAndView("watchlistItemForm" , model);
+    }
+
+    private WatchlistItem findWatchlistItemById(Integer id) {
+        for (WatchlistItem watchlistItem : watchlistItems) {
+            if (watchlistItem.getId().equals(id)) {
+                return watchlistItem;
+            }
+        }
+        return null;
+    }
 
     @GetMapping("/watchlist")
-    public ModelAndView getList() {
+    public ModelAndView getWatchlist() {
 
         String viewName = "watchlist";
         Map<String, Object> model = new HashMap<String, Object>();
-
-        watchlistItems.clear();
-        watchlistItems.add(new WatchlistItem("Lion King","8.5","high","Hakuna matata!"));
-        watchlistItems.add(new WatchlistItem("Frozen","7.5","medium","Let it go!"));
-        watchlistItems.add(new WatchlistItem("Cars","7.1","low","Go go go!"));
-        watchlistItems.add(new WatchlistItem("Wall-E","8.4","high","You are crying!"));
 
         model.put("watchlistItems", watchlistItems);
         model.put("numberOfMovies", watchlistItems.size());
 
         return new ModelAndView(viewName , model);
+    }
+
+    @PostMapping("/watchlistItemForm")
+    public ModelAndView submitWatchlistItemForm(WatchlistItem watchlistItem) {
+
+        WatchlistItem existingItem = findWatchlistItemById(watchlistItem.getId());
+
+        if (existingItem == null) {
+            watchlistItem.setId(index++);
+            watchlistItems.add(watchlistItem);
+        } else {
+            existingItem.setComment(watchlistItem.getComment());
+            existingItem.setPriority(watchlistItem.getPriority());
+            existingItem.setRating(watchlistItem.getRating());
+            existingItem.setTitle(watchlistItem.getTitle());
+        }
+
+        RedirectView redirect = new RedirectView();
+        redirect.setUrl("/watchlist");
+
+        return new ModelAndView(redirect);
     }
 }
